7592b738835399a80f0f8285e0ab7ef4aca8b0bb
Sahil Khanna <mail@sahilkhanna.org>
2019-06-05 23:25:56 +1000

    Cleanup

--- build/components/component_project_vars.mk
+++
@@ -1,8 +0,0 @@
-# Automatically generated build file. Do not edit.
-COMPONENT_INCLUDES += $(PROJECT_PATH)/components/include
-COMPONENT_LDFLAGS += -L$(BUILD_DIR_BASE)/components -lcomponents
-COMPONENT_LINKER_DEPS += 
-COMPONENT_SUBMODULES += 
-COMPONENT_LIBRARIES += components
-COMPONENT_LDFRAGMENTS += 
-component-components-build: 
--- build/dotstar/component_project_vars.mk
+++
@@ -1,8 +0,0 @@
-# Automatically generated build file. Do not edit.
-COMPONENT_INCLUDES += $(PROJECT_PATH)/components/dotstar/include
-COMPONENT_LDFLAGS += -L$(BUILD_DIR_BASE)/dotstar -ldotstar
-COMPONENT_LINKER_DEPS += 
-COMPONENT_SUBMODULES += 
-COMPONENT_LIBRARIES += dotstar
-COMPONENT_LDFRAGMENTS += 
-component-dotstar-build: 
